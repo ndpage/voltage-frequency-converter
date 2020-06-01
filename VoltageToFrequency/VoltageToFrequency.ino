54440d8cf0c2d6ff846745c452ef75214da0bee2
pag1fni <nathan.page@bosch.com>
2020-06-01 17:33:01 -0400

    Create new functions checkLimits and setOutputFrequency. Need to add code.

--- VoltageToFrequency/VoltageToFrequency.ino
+++ VoltageToFrequency/VoltageToFrequency.ino
@@ -1,15 +1,18 @@
 
+
 // Global variable declarations
 int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = 13;      // select the pin for the LED
 int freqOutputPin = 2;
 int sensorValue = 0; // variable to store the value coming from the sensor
 float freqSetPoint = 0; 
-float timeSetPoint = 0;
 
+float maxFreq = 500.0;
+float minFreq = 5.0;
 
-
-// Main setup function 
+/*  
+ *   Main set up functions 
+*/ 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -21,7 +24,9 @@ void setup() {
 
 }
 
-// Main loop
+/*  
+ *   Main loop 
+*/ 
 void loop() {
     // put your main code here, to run repeatedly:
 
@@ -29,15 +34,12 @@ void loop() {
   sensorValue = analogRead(sensorPin); // Returns an int value between 0-1023
 
     // Convert from volts to hertz
-  freqSetPoint = (512.0/1023)*sensorValue; 
-  freqSetPoint = 100.0;
+  freqSetPoint = (maxFreq/1023)*sensorValue; 
   
 freqSetPoint = checkLimits(freqSetPoint);
 
 printFloat(freqSetPoint);
 
-timeSetPoint = 1/freqSetPoint;
-timeSetPoint = timeSetPoint*1000; //convert time set point to miliseconds to use with delay()
  /* 
   digitalWrite(2, HIGH);
   delay(timeSetPoint/2);
@@ -46,42 +48,85 @@ timeSetPoint = timeSetPoint*1000; //convert time set point to miliseconds to use
   delay(timeSetPoint/2);
   */
 }
+//======= END OF MAIN LOOP  =======
 
+
+/*-----------------------------------------------------------------------------------------------
+ Name: checkLimits
+ Usage: Checks the calculated value and coerrces to the maximum or minimum limits
+        specified by the global variables maxFreq and minFreq, respectively
+ Author: Nathan Page 
+ -----------------------------------------------------------------------------------------------
+ */
 float checkLimits(float value){
-  if (value > 512) {
-    return 512.0;
+  if (value > maxFreq) {
+    return maxFreq;
   }
-  else if (value < 5) {
-    return 5.0;
+  else if (value < minFreq) {
+    return minFreq;
   }
   else return value;
-}
+}//-----------------------------------------------------------------------------------------------
 
-void updateSerial(int input){
-  Serial.println(input);
-  return ;
-}
+/*-----------------------------------------------------------------------------------------------
+ Function: setOutputFreq
+ Usage: Sets the output frequency of the specified digital output pin. Pulse width is set by the 
+        variable pulseWidth
+ Author: Nathan Page
 
+                      #####  INCOMPLETE #####
+ ------------------------------------------------------------------------------------------------
+ */
+void setOutputFreq(float frequencySetPoint) {
+  //const long timeInterval = 1000; // in milliseconds
+  static unsigned long lastTime = 0;
+  static unsigned int currentState=0;
+  static unsigned int prevState=0;
+  
+  float timeSetPoint = 0;
+  
+  unsigned long currentTime = millis(); // Get the current time
+
+// NEED TO DETERMINE MECHANISM FOR SETTING THE OUTPUT FREQUENCY 
+//  WHEN CALLING THIS FUNCTION FROM THE MAIN LOOP
+
+  timeSetPoint = 1/frequencySetPoint;
+  timeSetPoint = timeSetPoint*1000; //convert time set point to miliseconds 
+
+  if ( currentTime - lastTime > timeSetPoint) {
+    lastTime = currentTime;
+    // Set the digital pin HIGH or LOW depending on the frequency set point and previous state
+    switch(prevState){
+      case 0: digitalWrite(2, HIGH);
+              currentState = 1;
+              break;
+      case 1: digitalWrite(2, LOW);
+              currentState = 0;
+              break;
+      default: digitalWrite(2, LOW);
+               currentState = 0; 
+              break;
+    }
+    prevState = currentState;
+  }
+  
+}//-----------------------------------------------------
+
+/*-----------------------------------------------------------------------------------------------
+ Function: printFloat
+ Usage: Prints the float value passed to the function to the serial stream 
+        at a specified time interval (in ms)
+ Author: Nathan Page
+ -----------------------------------------------------------------------------------------------
+ */
 void printFloat(float valueToPrint) {
-  const long timeInterval = 1000;
+  const long timeInterval = 1000; // in milliseconds
   static unsigned long lastTime = 0;
   
-  unsigned long now = millis();
+  unsigned long now = millis(); // Get the current time
 
   if ( now - lastTime > timeInterval) {
     lastTime = now;
     Serial.println(valueToPrint);
   }
-}
-
-void printString(char* valueToPrint) {
-  const long timeInterval = 1000;
-  static unsigned long lastTime = 0;
-  
-  unsigned long now = millis();
-
-  if ( now - lastTime > timeInterval) {
-    lastTime = now;
-    Serial.println(valueToPrint);
-  }
-}
+}//-----------------------------------------------------------------------------------------------
